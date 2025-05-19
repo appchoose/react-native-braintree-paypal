fc34642723acbce6a728b4bbdff2636df9513b30
Hugo EXTRAT <extrat.h@gmail.com>
2025-05-19 13:04:42 +0200

    v0.3.4

--- ios/BraintreePaypal.mm
+++ ios/BraintreePaypal.mm
@@ -6,7 +6,8 @@ RCT_EXTERN_METHOD(showPayPal
                   : (NSString *)serverUrl amount
                   : (NSString *)amount shippingRequired
                   : (BOOL)shippingRequired currency
-                  : (NSString *)currency email appLink
+                  : (NSString *)currency appLink
+                  : (NSString *)appLink email
                   : (NSString *)email resolve
                   : (RCTPromiseResolveBlock)resolve reject
                   : (RCTPromiseRejectBlock)reject)
